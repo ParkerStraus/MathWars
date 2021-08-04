4d50bb7f15745357786b42bf83fe4818b6f988fa
pestydesperado248 <58351647+pestydesperado248@users.noreply.github.com>
2021-08-04 16:35:30 -0500

    Added Clickable object and "object permenance"

---
+++ Assets/Scripts/PointAndClickHandler.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Room
+{
+    public GameObject RoomObject;
+    public GameObject PreviousRoom;
+
+    public Room(GameObject RoomObject, GameObject PreviousRoom)
+    {
+        this.RoomObject = RoomObject;
+        this.PreviousRoom = PreviousRoom;
+    }
+
+    public GameObject GetRoomObject()
+    {
+        return RoomObject;
+    }
+
+    public GameObject GetPrevRoom()
+    {
+        return PreviousRoom;
+    }
+}
+
+public class PointAndClickHandler : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private Room[] Rooms;
+    [SerializeField] private Room CurrentRoom;
+    public PointAndClickObject[] RoomObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            if (hit)
+            {
+                Debug.Log(hit.collider.gameObject.name + " was touched by an angle");
+                hit.collider.gameObject.GetComponent<Clickable>().TriggerEvent();
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            foreach (Room room in Rooms)
+            {
+                room.GetRoomObject().SetActive(false);
+            }
+            CurrentRoom.GetPrevRoom().SetActive(true);
+        }
+    }
+
+    public void SwapRoom(int roomNum)
+    {
+        foreach(Room room in Rooms)
+        {
+            room.GetRoomObject().SetActive(false);
+        }
+        Rooms[roomNum].GetRoomObject().SetActive(true);
+        CurrentRoom = Rooms[roomNum];
+    }
+
+    public void SetRoomObject(int i)
+    {
+        RoomObjects[i].SwapActiveSetting();
+    }
+}
---
+++ Assets/Scripts/PointAndClickObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PointAndClickObject
+{
+    public GameObject[]  Instances;
+    public bool ActiveSetting;
+
+    public PointAndClickObject(GameObject[] Instances, bool ActiveSetting)
+    {
+        this.Instances = Instances;
+        this.ActiveSetting = ActiveSetting;
+    }
+
+    public bool GetActiveSetting()
+    {
+        return ActiveSetting;
+    }
+
+    public void SwapActiveSetting()
+    {
+        ActiveSetting = !ActiveSetting;
+        foreach (GameObject instance in Instances)
+        {
+            instance.SetActive(ActiveSetting);
+        }
+    }
+
+    public GameObject[] GetInstances()
+    {
+        return Instances;
+    }
+
+    public void SetInstances(GameObject[] newinstances)
+    {
+        Instances = newinstances;
+    }
+}
